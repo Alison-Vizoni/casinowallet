a90f5fec8977a9971225465a3dad7335f2bb0afc
Alison-Vizoni <alison.vizoni@gmail.com>
2024-06-20 22:00:57 -0300

    chore: first commit

---
+++ src/main/java/com/casinowallet/casinowallet/CasinoWalletApplication.java
@@ -0,0 +1,13 @@
+package com.casinowallet.casinowallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasinoWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasinoWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casinowallet/casinowallet/CasinoWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casinowallet.casinowallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasinoWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
