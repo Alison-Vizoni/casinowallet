5eabe62a777819e981e060f7a96bc1ff6e2fb52e
Alison-Vizoni <alison.vizoni@gmail.com>
2024-06-27 22:03:14 -0300

    feat: create custom exception handler

---
+++ src/main/java/com/casinowallet/casinowallet/controller/exception/StandardError.java
@@ -0,0 +1,23 @@
+package com.casinowallet.casinowallet.controller.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StandardError implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Instant timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+}
