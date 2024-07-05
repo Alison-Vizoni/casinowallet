800069c044692190c0982557fe8412f66ceb5e2c
Alison-Vizoni <alison.vizoni@gmail.com>
2024-07-04 22:00:35 -0300

    fix: add missing annotation

--- src/main/java/com/casinowallet/casinowallet/repository/ProviderRepository.java
+++ src/main/java/com/casinowallet/casinowallet/repository/ProviderRepository.java
@@ -2,9 +2,11 @@ package com.casinowallet.casinowallet.repository;
 
 import com.casinowallet.casinowallet.models.entity.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ProviderRepository extends JpaRepository<Provider, Long> {
     Optional<Provider> findByStrId(String strId);
 }
