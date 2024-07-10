bc15d340bac630adfe393426db7e9e054173225a
Alison-Vizoni <alison.vizoni@gmail.com>
2024-07-09 21:47:35 -0300

    chore: add missing value attribute

--- src/main/java/com/casinowallet/casinowallet/controller/GameController.java
+++ src/main/java/com/casinowallet/casinowallet/controller/GameController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/game")
+@RequestMapping(value = "/game")
 public class GameController {
 
     @Autowired
--- src/main/java/com/casinowallet/casinowallet/controller/PlayerController.java
+++ src/main/java/com/casinowallet/casinowallet/controller/PlayerController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/player")
+@RequestMapping(value = "/player")
 public class PlayerController {
 
     @Autowired
